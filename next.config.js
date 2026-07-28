d1d22e642d0e3cbf61adda7b44988e44f1639549
andika candra prasetya <baksocloud@gmail.com>
2026-07-28 06:55:16 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,15 @@ const nextConfig = {
       },
     ],
   },
+  // ✅ Tambahkan ini untuk proxy API
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.ndikacunk.my.id/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
